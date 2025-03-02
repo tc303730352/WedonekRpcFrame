3b27375f97b3fba61e4ba736c1ce498331ae61ca
quanquan <303730352@qq.com>
2025-03-02 19:24:13 +0800

    更新本地事务开始的位置

--- Source/Basic/WeDonekRpc.Client/Tran/TranState.cs
+++ Source/Basic/WeDonekRpc.Client/Tran/TranState.cs
@@ -2,7 +2,6 @@
 using WeDonekRpc.Client.Collect;
 using WeDonekRpc.Client.Config;
 using WeDonekRpc.Client.Interface;
-using WeDonekRpc.Client.Log;
 using WeDonekRpc.Helper;
 using WeDonekRpc.Model;
 using WeDonekRpc.Model.Tran.Model;
@@ -11,14 +10,13 @@ namespace WeDonekRpc.Client.Tran
 {
     internal class TranStateVal : ICurTranState
     {
-        private readonly CurTranState _State;
         private IDisposable _Ass;
 
-        public TranStateVal (long tranId, ITranTemplate template, string body)
+        public TranStateVal ( long tranId, ITranTemplate template, string body )
         {
             this.Template = template;
             this.Body = new TranSource(body);
-            this._State = new CurTranState
+            this.Source = new CurTranState
             {
                 TranId = tranId,
                 RegionId = RpcStateCollect.ServerConfig.RegionId,
@@ -26,11 +24,11 @@ namespace WeDonekRpc.Client.Tran
                 OverTime = DateTime.Now.AddSeconds(WebConfig.RpcConfig.TranOverTime).ToLong()
             };
         }
-        public TranStateVal (long tranId, ITranTemplate template, CurTranState state, string body)
+        public TranStateVal ( long tranId, ITranTemplate template, CurTranState state, string body )
         {
             this.Body = new TranSource(body);
             this.Template = template;
-            this._State = new CurTranState
+            this.Source = new CurTranState
             {
                 TranId = tranId,
                 RegionId = state.RegionId,
@@ -38,11 +36,11 @@ namespace WeDonekRpc.Client.Tran
                 OverTime = state.OverTime
             };
         }
-        public TranStateVal (long tranId, ITranTemplate template, CurTranState state, ITranSource source)
+        public TranStateVal ( long tranId, ITranTemplate template, CurTranState state, ITranSource source )
         {
             this.Body = source;
             this.Template = template;
-            this._State = new CurTranState
+            this.Source = new CurTranState
             {
                 TranId = tranId,
                 RegionId = state.RegionId,
@@ -50,72 +48,64 @@ namespace WeDonekRpc.Client.Tran
                 OverTime = state.OverTime
             };
         }
-        public TranStateVal (CurTranState state)
+        public TranStateVal ( CurTranState state )
         {
-            this._State = state;
+            this.Source = state;
             this.Body = new TranSource();
         }
         public void BeginTran ()
         {
-            if (this.Template.TranMode == RpcTranMode.TwoPC)
+            if ( this.Template.TranMode == RpcTranMode.TwoPC )
             {
-                try
-                {
-                    this._Ass = this.Template.BeginTran(this);
-                }
-                catch (Exception e)
-                {
-                    RpcLogSystem.AddErrorLog("启动事务错误", e);
-                    throw new ErrorException("rpc.tran.begin.fail");
-                }
+                this._Ass = this.Template.BeginTran(this);
             }
         }
-        public CurTranState Source => this._State;
+        public CurTranState Source { get; }
         /// <summary>
         /// 事务Id
         /// </summary>
-        public long TranId => this._State.TranId;
+        public long TranId => this.Source.TranId;
 
         /// <summary>
         /// 事务协调服务所在区
         /// </summary>
-        public int RegionId => this._State.RegionId;
+        public int RegionId => this.Source.RegionId;
         /// <summary>
         /// 事务协调服务所在集群
         /// </summary>
-        public long RpcMerId => this._State.RpcMerId;
+        public long RpcMerId => this.Source.RpcMerId;
         /// <summary>
         /// 事务超时时间
         /// </summary>
-        public long OverTime => this._State.OverTime;
+        public long OverTime => this.Source.OverTime;
 
         public ITranTemplate Template { get; }
 
         public ITranSource Body { get; }
 
-        public CurTranState TranState => this._State;
+        public CurTranState TranState => this.Source;
 
-        public override bool Equals (object obj)
+        public override bool Equals ( object obj )
         {
-            if (obj is ICurTran i)
+            if ( obj is ICurTran i )
             {
-                return i.TranId == this._State.TranId;
+                return i.TranId == this.Source.TranId;
             }
             return false;
         }
 
-        public bool Equals (ICurTran other)
+        public bool Equals ( ICurTran other )
         {
-            if (other == null)
+            if ( other == null )
             {
                 return false;
             }
-            return other.TranId == this._State.TranId;
+            return other.TranId == this.Source.TranId;
         }
 
         public override int GetHashCode ()
         {
-            return this._State.TranId.GetHashCode();
+            return this.Source.TranId.GetHashCode();
         }
 
         public void Dispose ()
--- Source/Basic/WeDonekRpc.Client/TranService/RpcTranService.cs
+++ Source/Basic/WeDonekRpc.Client/TranService/RpcTranService.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Concurrent;
+﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Net;
 using System.Threading;
@@ -6,6 +7,7 @@ using WeDonekRpc.Client.Collect;
 using WeDonekRpc.Client.Config;
 using WeDonekRpc.Client.Interface;
 using WeDonekRpc.Client.Ioc;
+using WeDonekRpc.Client.Log;
 using WeDonekRpc.Client.Model;
 using WeDonekRpc.Client.RpcSysEvent;
 using WeDonekRpc.Client.Tran;
@@ -21,10 +23,7 @@ namespace WeDonekRpc.Client.TranService
     internal class RpcTranService : IRpcTranService
     {
         private static readonly AsyncLocal<ICurTranState> _SourceTran = new AsyncLocal<ICurTranState>();
-        internal static ICurTranState SourceTran
-        {
-            get => _SourceTran.Value;
-        }
+        internal static ICurTranState SourceTran => _SourceTran.Value;
         private static readonly AsyncLocal<ICurTranState> _CurTran = new AsyncLocal<ICurTranState>();
 
         private static readonly ConcurrentDictionary<string, ITranTemplate> _Tran = new ConcurrentDictionary<string, ITranTemplate>();
@@ -38,32 +37,29 @@ namespace WeDonekRpc.Client.TranService
             RemoteSysEvent.AddEvent<TranRollback>("Rpc_TranRollback", _TranRollback);
             RemoteSysEvent.AddEvent<TranCommit>("Rpc_TranCommit", _TranSubmit);
         }
-        public static long? TranId
-        {
-            get => RpcTranService._SourceTran.Value?.TranId;
-        }
+        public static long? TranId => RpcTranService._SourceTran.Value?.TranId;
         public static CurTranState GetTranSource ()
         {
             return RpcTranService._SourceTran.Value?.Source;
         }
-        private static TcpRemoteReply _TranSubmit (TranCommit obj, MsgSource source)
+        private static TcpRemoteReply _TranSubmit ( TranCommit obj, MsgSource source )
         {
-            if (_Tran.TryGetValue(obj.TranName, out ITranTemplate tran) && tran.TranMode != RpcTranMode.NoReg)
+            if ( _Tran.TryGetValue(obj.TranName, out ITranTemplate tran) && tran.TranMode != RpcTranMode.NoReg )
             {
                 ICurTran cur = new CurState(obj, source);
-                using (IocScope scope = RpcClient.Ioc.CreateScore("Tran_Commit"))
+                using ( IocScope scope = RpcClient.Ioc.CreateScore("Tran_Commit") )
                 {
                     tran.Commit(cur);
                 }
             }
             return new TcpRemoteReply(new BasicRes());
         }
-        private static TcpRemoteReply _TranRollback (TranRollback obj, MsgSource source)
+        private static TcpRemoteReply _TranRollback ( TranRollback obj, MsgSource source )
         {
-            if (_Tran.TryGetValue(obj.TranName, out ITranTemplate tran) && tran.TranMode != RpcTranMode.NoReg)
+            if ( _Tran.TryGetValue(obj.TranName, out ITranTemplate tran) && tran.TranMode != RpcTranMode.NoReg )
             {
                 ICurTran cur = new CurState(obj, source);
-                using (IocScope scope = RpcClient.Ioc.CreateScore("Tran_Rollback"))
+                using ( IocScope scope = RpcClient.Ioc.CreateScore("Tran_Rollback") )
                 {
                     tran.Rollback(cur);
                 }
@@ -71,7 +67,7 @@ namespace WeDonekRpc.Client.TranService
             return new TcpRemoteReply(new BasicRes());
         }
 
-        private static bool _ApplyTran (ITranTemplate template, string data, out ICurTranState tran, out ICurTranState oldTran)
+        private static bool _ApplyTran ( ITranTemplate template, string data, out ICurTranState tran, out ICurTranState oldTran )
         {
             ApplyTran obj = new ApplyTran
             {
@@ -81,32 +77,35 @@ namespace WeDonekRpc.Client.TranService
                 SubmitJson = data,
                 TimeOut = WebConfig.RpcConfig.TranOverTime,
             };
-            if (!RemoteCollect.Send(obj, out string error))
+            if ( !RemoteCollect.Send(obj, out string error) )
             {
                 throw new ErrorException(error);
             }
             tran = new TranStateVal(obj.TranId, template, data);
-            tran.BeginTran();
-            oldTran = RpcTranService._SourceTran.Value;
-            RpcTranService._SourceTran.Value = tran;
-            RpcTranService._CurTran.Value = tran;
-            return false;
+            if ( _BeginLocalTran(tran, out error) )
+            {
+                oldTran = RpcTranService._SourceTran.Value;
+                RpcTranService._SourceTran.Value = tran;
+                RpcTranService._CurTran.Value = tran;
+                return false;
+            }
+            throw new ErrorException(error);
         }
 
 
-        internal static bool ApplyTran (string tranName, string data, bool isinherit, out ICurTranState tran, out ICurTranState oldTran)
+        internal static bool ApplyTran ( string tranName, string data, bool isinherit, out ICurTranState tran, out ICurTranState oldTran )
         {
-            if (isinherit && RpcTranService._SourceTran.Value != null)
+            if ( isinherit && RpcTranService._SourceTran.Value != null )
             {
                 oldTran = null;
-                if (RpcTranService._SourceTran.Value is IRpcVirtuallyTransaction virtually)
+                if ( RpcTranService._SourceTran.Value is IRpcVirtuallyTransaction virtually )
                 {
                     virtually.InitTran(tranName);
                 }
                 tran = RpcTranService._CurTran.Value;
                 return true;
             }
-            else if (!_Tran.TryGetValue(tranName, out ITranTemplate template))
+            else if ( !_Tran.TryGetValue(tranName, out ITranTemplate template) )
             {
                 throw new ErrorException("rpc.tran.no.reg");
             }
@@ -116,22 +115,22 @@ namespace WeDonekRpc.Client.TranService
             }
         }
 
-        internal static void SubmitTran (ICurTranState tran, ICurTranState source)
+        internal static void SubmitTran ( ICurTranState tran, ICurTranState source )
         {
             _SetTran(source);
             _SubmitTran(tran);
-            if (tran.Template.TranMode != RpcTranMode.NoReg)
+            if ( tran.Template.TranMode != RpcTranMode.NoReg )
             {
                 tran.Template.Commit(tran);
             }
         }
-        private static void _SubmitTran (ICurTranState tran)
+        private static void _SubmitTran ( ICurTranState tran )
         {
             SubmitTran obj = new SubmitTran
             {
                 TranId = tran.TranId
             };
-            if (!RemoteCollect.Send(tran.RpcMerId, tran.RegionId, obj, out string error))
+            if ( !RemoteCollect.Send(tran.RpcMerId, tran.RegionId, obj, out string error) )
             {
                 throw new ErrorException(error)
                 {
@@ -144,19 +143,19 @@ namespace WeDonekRpc.Client.TranService
                 };
             }
         }
-        private static void _SetTran (ICurTranState tran)
+        private static void _SetTran ( ICurTranState tran )
         {
             RpcTranService._SourceTran.Value = tran;
             RpcTranService._CurTran.Value = tran;
         }
 
-        private static void _Rollback (ICurTranState tran)
+        private static void _Rollback ( ICurTranState tran )
         {
             RollbackTran obj = new RollbackTran
             {
                 TranId = tran.TranId
             };
-            if (!RemoteCollect.Send(tran.RpcMerId, tran.RegionId, obj, out string error))
+            if ( !RemoteCollect.Send(tran.RpcMerId, tran.RegionId, obj, out string error) )
             {
                 throw new ErrorException(error)
                 {
@@ -171,19 +170,19 @@ namespace WeDonekRpc.Client.TranService
         }
 
 
-        internal static void RollbackTran (ICurTranState tran, ICurTranState source)
+        internal static void RollbackTran ( ICurTranState tran, ICurTranState source )
         {
             _SetTran(source);
             _Rollback(tran);
-            if (tran.Template.TranMode != RpcTranMode.NoReg)
+            if ( tran.Template.TranMode != RpcTranMode.NoReg )
             {
                 tran.Template.Rollback(tran);
             }
         }
 
-        internal static ICurTranState AddTranLog (CurTranState state, ITranSource source, string tranName)
+        internal static ICurTranState AddTranLog ( CurTranState state, ITranSource source, string tranName )
         {
-            if (!_Tran.TryGetValue(tranName, out ITranTemplate template))
+            if ( !_Tran.TryGetValue(tranName, out ITranTemplate template) )
             {
                 throw new ErrorException("rpc.tran.no.reg");
             }
@@ -200,57 +199,37 @@ namespace WeDonekRpc.Client.TranService
                     SubmitJson = source.Body
                 }
             };
-            if (!RemoteCollect.Send(state.RpcMerId, state.RegionId, log, out long id, out string error))
+            if ( !RemoteCollect.Send(state.RpcMerId, state.RegionId, log, out long id, out string error) )
             {
                 throw new ErrorException(error);
             }
             ICurTranState cur = new TranStateVal(id, template, state, source);
-            cur.BeginTran();
-            RpcTranService._CurTran.Value = cur;
-            return cur;
-        }
-        internal static bool AddQueueTranLog (RemoteMsg msg)
-        {
-            if (!_Tran.TryGetValue(msg.MsgKey, out ITranTemplate template))
+            if ( _BeginLocalTran(cur, out error) )
             {
-                RpcTranService._SourceTran.Value = new VirtuallyTransaction(msg.TcpMsg.Tran, msg.MsgBody);
-                return true;
-            }
-            CurTranState tran = msg.TcpMsg.Tran;
-            AddTranLog log = new AddTranLog
-            {
-                TranId = tran.TranId,
-                TranLog = new TranLogDatum
-                {
-                    TranMode = template.TranMode,
-                    Dictate = msg.MsgKey,
-                    RpcMerId = RpcStateCollect.RpcMerId,
-                    ServerId = RpcStateCollect.ServerId,
-                    SystemType = RpcStateCollect.LocalConfig.SystemType,
-                    SubmitJson = msg.MsgBody
-                }
-            };
-            if (RemoteCollect.Send(tran.RpcMerId, tran.RegionId, log, out long id, out string error))
-            {
-                ICurTranState cur = new TranStateVal(id, template, tran, msg.MsgBody);
-                cur.BeginTran();
-                RpcTranService._SourceTran.Value = new TranStateVal(tran);
                 RpcTranService._CurTran.Value = cur;
-                return true;
+                return cur;
             }
-            else if (error.StartsWith("rpc.tran."))
-            {
-                return false;
-            }
-            else
-            {
-                throw new ErrorException(error);
-            }
+            throw new ErrorException(error);
         }
 
-        internal static bool AddTranLog (RemoteMsg msg, out ICurTranState cur, out string error)
+        private static bool _BeginLocalTran ( ICurTranState cur, out string error )
         {
-            if (!_Tran.TryGetValue(msg.MsgKey, out ITranTemplate template))
+            try
+            {
+                cur.BeginTran();
+                error = null;
+                return true;
+            }
+            catch ( Exception e )
+            {
+                RpcLogSystem.AddErrorLog("启动事务错误", e);
+                error = "rpc.tran.begin.fail";
+                return false;
+            }
+        }
+        internal static bool AddTranLog ( RemoteMsg msg, out ICurTranState cur, out string error )
+        {
+            if ( !_Tran.TryGetValue(msg.MsgKey, out ITranTemplate template) )
             {
                 RpcTranService._SourceTran.Value = new VirtuallyTransaction(msg.TcpMsg.Tran, msg.MsgBody);
                 error = null;
@@ -271,61 +250,63 @@ namespace WeDonekRpc.Client.TranService
                     SubmitJson = msg.MsgBody
                 }
             };
-            if (RemoteCollect.Send(tran.RpcMerId, tran.RegionId, log, out long id, out error))
+            if ( RemoteCollect.Send(tran.RpcMerId, tran.RegionId, log, out long id, out error) )
             {
                 cur = new TranStateVal(id, template, tran, msg.MsgBody);
-                cur.BeginTran();
-                RpcTranService._SourceTran.Value = new TranStateVal(tran);
-                RpcTranService._CurTran.Value = cur;
-                return true;
+                if ( _BeginLocalTran(cur, out error) )
+                {
+                    RpcTranService._SourceTran.Value = new TranStateVal(tran);
+                    RpcTranService._CurTran.Value = cur;
+                    return true;
+                }
             }
             cur = null;
             return false;
         }
-        public static void SetTranExtend (ICurTranState tran, string extend)
+        public static void SetTranExtend ( ICurTranState tran, string extend )
         {
             SetTranExtend set = new SetTranExtend
             {
                 TranId = tran.TranId,
                 Extend = extend
             };
-            if (!RemoteCollect.Send(tran.RpcMerId, tran.RegionId, set, out string error))
+            if ( !RemoteCollect.Send(tran.RpcMerId, tran.RegionId, set, out string error) )
             {
                 throw new ErrorException(error);
             }
             tran.Body.Extend = extend;
         }
-        public void SetTranExtend<T> (T extend) where T : class
+        public void SetTranExtend<T> ( T extend ) where T : class
         {
-            if (RpcTranService._CurTran.Value != null)
+            if ( RpcTranService._CurTran.Value != null )
             {
                 SetTranExtend(RpcTranService._CurTran.Value, extend.ToJson());
             }
         }
 
 
-        public void SetTranExtend (string extend)
+        public void SetTranExtend ( string extend )
         {
-            if (RpcTranService._CurTran.Value != null)
+            if ( RpcTranService._CurTran.Value != null )
             {
                 SetTranExtend(RpcTranService._CurTran.Value, extend);
             }
         }
 
-        public void SetTranExtend (Dictionary<string, object> extend)
+        public void SetTranExtend ( Dictionary<string, object> extend )
         {
-            if (RpcTranService._CurTran.Value != null)
+            if ( RpcTranService._CurTran.Value != null )
             {
                 SetTranExtend(RpcTranService._CurTran.Value, extend.ToJson());
             }
         }
-        public static void RegTran (string name, ITranTemplate template)
+        public static void RegTran ( string name, ITranTemplate template )
         {
-            if (_Tran.ContainsKey(name))
+            if ( _Tran.ContainsKey(name) )
             {
                 throw new WebException("rpc.tran.repeat");
             }
-            else if (!_Tran.TryAdd(name, template))
+            else if ( !_Tran.TryAdd(name, template) )
             {
                 throw new WebException("rpc.tran.reg.fail");
             }
@@ -335,13 +316,13 @@ namespace WeDonekRpc.Client.TranService
         public TransactionStatus GetTranState ()
         {
             ICurTranState tran = RpcTranService._CurTran.Value;
-            if (tran == null)
+            if ( tran == null )
             {
                 throw new ErrorException("rpc.tran.no.find");
             }
             return this._GetTranState(tran.TranId, tran.RpcMerId, tran.RegionId);
         }
-        public bool GetTranState (CurTranState state, out TransactionStatus status, out string error)
+        public bool GetTranState ( CurTranState state, out TransactionStatus status, out string error )
         {
             GetTranState obj = new GetTranState
             {
@@ -349,13 +330,13 @@ namespace WeDonekRpc.Client.TranService
             };
             return RemoteCollect.Send(state.RpcMerId, state.RegionId, obj, out status, out error);
         }
-        private TransactionStatus _GetTranState (long tranId, long rpcMerId, int regionId)
+        private TransactionStatus _GetTranState ( long tranId, long rpcMerId, int regionId )
         {
             GetTranState obj = new GetTranState
             {
                 TranId = tranId
             };
-            if (!RemoteCollect.Send(rpcMerId, regionId, obj, out TransactionStatus status, out string error))
+            if ( !RemoteCollect.Send(rpcMerId, regionId, obj, out TransactionStatus status, out string error) )
             {
                 throw new ErrorException(error);
             }
@@ -369,7 +350,7 @@ namespace WeDonekRpc.Client.TranService
         }
         public static void ReceiveEnd ()
         {
-            if (RpcTranService._CurTran.Value != null)
+            if ( RpcTranService._CurTran.Value != null )
             {
                 _SetTran(null);
             }
